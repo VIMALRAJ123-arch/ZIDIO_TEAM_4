2a2f16787103d39f04c608c640e1eac351d369a0
aseel <aseel26005@gmail.com>
2025-04-21 18:57:01 +0530

    updated starry background

--- components/starry-background.tsx
+++ components/starry-background.tsx
@@ -14,7 +14,7 @@ function MovingStars() {
     }
   })
 
-  return <Stars ref={starsRef} radius={100} depth={50} count={5000} factor={4} saturation={0} fade speed={1} />
+  return <Stars ref={starsRef} radius={100} depth={50} count={5000} factor={8} saturation={0} fade speed={1} />
 }
 
 export default function StarryBackground() {
